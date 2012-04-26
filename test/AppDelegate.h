352865da318c048c9537fe316fa4c97e09e132ef
Ran Tao <ran@ephexi.com>
2012-04-25 21:09:54 -0700

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Ran Tao on 4.25.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Ran Tao on 4.25.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
